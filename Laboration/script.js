91110087b0aaa7da67db8f4b935c370354100e07
speelgren <97473666+speelgren@users.noreply.github.com>
2022-04-21 16:12:09 +0200

    scuffed början

--- Laboration/script.js
+++ Laboration/script.js
@@ -5,54 +5,114 @@
 'use strict';
 
 window.addEventListener('load', init);
+document.querySelector('.navbar-brand').addEventListener('click', function() {
+
+  location.reload();
+});
 
 function init() {
 
-  document.querySelector('#form').addEventListener('submit', handleSubmit)
+  document.querySelector('#form').addEventListener('submit', handleSubmit);
+  document.querySelector('#content').classList.add('d-flex', 'flex-wrap', 'justify-content-center');
 }
 
 function handleSubmit(e) {
 
-  let searchValue = document.querySelector('#search').value;
-
   e.preventDefault();
+  let searchValue = document.querySelector('#search').value;
   document.querySelector('#content').innerHTML = null;
+
   search(searchValue.toLowerCase(), document.querySelector('#content'));
 }
 
 function search(query, content) {
 
   window.fetch('https://pokeapi.co/api/v2/pokemon/' + encodeURIComponent(query)).then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data.species.name);
 
+    if(query == '') {
+
+      document.querySelector('.card').classList.add('d-none');
+    }
+
+    if(!response.ok) {
+
+      felmeddelande();
+    }
+
+    else {
+
+      return response.json();
+    }
+  }).then(function(data) {
+
+      //Image card
       let card = document.createElement('div');
-      card.style.width = '20rem';
-      card.style.border = '1px solid black';
+      card.style.width = '25rem';
       card.classList.add('card');
       content.appendChild(card);
 
       let cardImage = document.createElement('img');
       cardImage.classList.add('card-image-top');
-      cardImage.src = data.sprites.other.home.front_default;
-      cardImage.alt = data.species.name;
+      cardImage.src = data.sprites.other['official-artwork'].front_default;
+      cardImage.alt = data.species.name + ' pokemon';
+      cardImage.style.backgroundColor = '#A8DADC';
       card.appendChild(cardImage);
 
       let cardBody = document.createElement('div');
       cardBody.classList.add('card-body');
+      cardBody.style.backgroundColor = '#A8DADC';
       card.appendChild(cardBody);
 
-      let cardTitle = document.createElement('h5');
+      let cardTitle = document.createElement('h4');
       cardTitle.classList.add('card-title');
       let cardTitleNode = document.createTextNode(data.species.name.toUpperCase());
+      cardTitle.style.textAlign = 'center';
       cardTitle.appendChild(cardTitleNode);
-
-      //let cardParagraph = document.createElement('p');
-      //cardParagraph.classList.add('card-text');
-      //let cardParagraphNode = document.createTextNode(data.);
-
       cardBody.appendChild(cardTitle);
+
+      // Information card
+      let infoCard = document.createElement('div');
+      infoCard.style.width = '25rem';
+      infoCard.classList.add('card');
+      content.appendChild(infoCard);
+
+      let infoCardBody = document.createElement('div');
+      infoCardBody.classList.add('card-body');
+      infoCardBody.style.backgroundColor = '#F1FAEE';
+      infoCard.appendChild(infoCardBody);
+
+      let infoCardTitle = document.createElement('h1');
+      infoCardTitle.classList.add('card-text');
+      infoCardTitle.style.textAlign = 'center';
+      infoCardBody.appendChild(infoCardTitle);
+      let infoCardTitleNode = document.createTextNode('Information');
+      infoCardTitle.appendChild(infoCardTitleNode);
+
+      let card2TextNode = document.createTextNode(data.species.name);
+  }).catch((error) => {
+
+    console.log(error);
   });
+}
+
+function felmeddelande() {
+
+  let felCard = document.createElement('div');
+  felCard.style.width = '25rem';
+  felCard.classList.add('card');
+  content.appendChild(felCard);
+
+  let felCardBody = document.createElement('div');
+  felCardBody.classList.add('card-body');
+  felCardBody.style.backgroundColor = '#A8DADC';
+  felCard.appendChild(felCardBody);
+
+  let felCardTitle = document.createElement('h4');
+  felCardTitle.classList.add('card-title');
+  let felCardTitleNode = document.createTextNode('Vänligen sök igen.');
+  felCardTitle.style.textAlign = 'center';
+  felCardTitle.appendChild(felCardTitleNode);
+  felCardBody.appendChild(felCardTitle);
+
+  document.querySelector('.card')[1].classList.add('d-none');
 }
