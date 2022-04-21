41a446194d934be05dc32c0d06a34bf1ddf565e8
speelgren <97473666+speelgren@users.noreply.github.com>
2022-04-21 18:22:53 +0200

    scuffed information

--- Laboration/script.js
+++ Laboration/script.js
@@ -1,5 +1,6 @@
 /*
  * https://pokeapi.co/api/v2/pokemon/
+ * https://coolors.co/palette/e63946-f1faee-a8dadc-457b9d-1d3557
 */
 
 'use strict';
@@ -36,7 +37,7 @@ function search(query, content) {
 
     if(!response.ok) {
 
-      felmeddelande();
+      felmeddelande(query);
     }
 
     else {
@@ -65,7 +66,7 @@ function search(query, content) {
 
       let cardTitle = document.createElement('h4');
       cardTitle.classList.add('card-title');
-      let cardTitleNode = document.createTextNode(data.species.name.toUpperCase());
+      let cardTitleNode = document.createTextNode(data.name.toUpperCase()  + ' (' + data.id + ')');
       cardTitle.style.textAlign = 'center';
       cardTitle.appendChild(cardTitleNode);
       cardBody.appendChild(cardTitle);
@@ -82,34 +83,57 @@ function search(query, content) {
       infoCard.appendChild(infoCardBody);
 
       let infoCardTitle = document.createElement('h1');
-      infoCardTitle.classList.add('card-text');
+      infoCardTitle.classList.add('card-title');
       infoCardTitle.style.textAlign = 'center';
       infoCardBody.appendChild(infoCardTitle);
       let infoCardTitleNode = document.createTextNode('Information');
       infoCardTitle.appendChild(infoCardTitleNode);
 
-      let card2TextNode = document.createTextNode(data.species.name);
+      let i = Math.floor(Math.random() * data.moves.length);
+      let j = Math.floor(Math.random() * data.moves.length);
+      let favMove = data.moves[i].move.name;
+      let leastFavMove = data.moves[j].move.name;
+
+      let infoCardHeightP = document.createElement('pre');
+      infoCardHeightP.classList.add('card-text');
+      let infoCardHeightTextNode = document.createTextNode(
+        '\n' +
+        'type: ' + data.types[0].type.name + '\n' +
+        'height: ' + data.height * 10 + 'cm' + '\n' +
+        'weight: ' + data.weight / 10 + 'kg' + '\n\n' +
+        'favourite move: ' + favMove + '\n' +
+        'least favourite move: ' + leastFavMove +
+        '\n\n'
+        );
+      infoCardHeightP.appendChild(infoCardHeightTextNode);
+      infoCardBody.appendChild(infoCardHeightP);
   }).catch((error) => {
 
     console.log(error);
   });
 }
 
-function felmeddelande() {
+function felmeddelande(query) {
 
   let felCard = document.createElement('div');
   felCard.style.width = '25rem';
   felCard.classList.add('card');
   content.appendChild(felCard);
 
+  let felCardImage = document.createElement('img');
+  felCardImage.classList.add('card-image-top');
+  felCardImage.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png';
+  felCardImage.style.backgroundColor = '#A8DADC';
+  felCard.appendChild(felCardImage);
+
   let felCardBody = document.createElement('div');
   felCardBody.classList.add('card-body');
   felCardBody.style.backgroundColor = '#A8DADC';
   felCard.appendChild(felCardBody);
 
-  let felCardTitle = document.createElement('h4');
+  let felCardTitle = document.createElement('h5');
   felCardTitle.classList.add('card-title');
-  let felCardTitleNode = document.createTextNode('Vänligen sök igen.');
+  let felCardTitleNode = document.createTextNode(`wops! "${query}"` + ' finns ej i databasen: vänligen sök igen.');
   felCardTitle.style.textAlign = 'center';
   felCardTitle.appendChild(felCardTitleNode);
   felCardBody.appendChild(felCardTitle);
