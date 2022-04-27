df8770510c30b24515e9e36ec79d06b6b3fce5d3
speelgren <97473666+speelgren@users.noreply.github.com>
2022-04-27 13:27:59 +0200

    xml

--- Laboration XML/OMDb/script.js
+++ Laboration XML/OMDb/script.js
@@ -18,9 +18,7 @@ function searchValue(e) {
 
 function search(query) {
 
-  let apikey = '?apikey=ecb78f37&';
-
-  window.fetch('http://www.omdbapi.com/' + apikey + 't=' + query + '&r=xml')
+  window.fetch('http://www.omdbapi.com/?apikey=ecb78f37&t=' + query + '&r=xml')
   .then(function(response) {
 
     return response.text();
@@ -28,43 +26,17 @@ function search(query) {
 
     let parser = new window.DOMParser();
     let xmlDOM = parser.parseFromString(data, 'application/xml');
+    let movie = xmlDOM.querySelectorAll('movie');
 
-    let ul = document.createElement('ul');
-    ul.classList.add('list-group');
-    let main = document.querySelector('main');
+    let movieValue = movie[0];
+    let title = movieValue.getAttribute('title');
+    let genre = movieValue.getAttribute('genre');
+    let actors = movieValue.getAttribute('actors');
+    console.log(title, genre, actors);
 
-    let li = document.createElement('li');
-    li.classList.add('list-group-item');
 
-    let titleNode = document.createElement('h5');
-    let title = xmlDOM.querySelector('title');
-    let titleTextNode = document.createTextNode(title);
-    titleNode.appendChild(titleTextNode);
-    li.appendChild(titleNode);
 
-    let p = document.createElement('p');
-    let plot = xmlDOM.querySelector('plot');
-    let plotTextNode = document.createTextNode(plot);
-    p.appendChild(plotTextNode);
-    li.appendChild(p);
-    console.log(plot);
-
-    ul.appendChild(li);
-    main.appendChild(ul);
-
-    console.log(data);
-
-    /*
-
-    let h1 = document.createElement('h1');
-    let title = document.createTextNode(data.Title);
-    let p = document.createElement('p');
-    let actor = document.createTextNode(data.Actors);
-    p.appendChild(actor);
-    h1.appendChild(title);
-
-    document.querySelector('#content').appendChild(h1);
-    document.querySelector('#content').appendChild(p);
-    */
+  }).catch(function(error) {
+    console.log(error);
   })
 }
